52ac9cac3b2f3f9654257d944848bd09a39aceb9
Syedanas-ctrl <59249245+Syedanas-ctrl@users.noreply.github.com>
2022-01-03 02:31:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 //left hand side image
 let randomNumber1 = Math.floor(Math.random()*(6-1+1))+1;
-document.querySelector(".img1").setAttribute("src","./images/dice"+randomNumber1+".png");
+document.querySelector(".img1").setAttribute("src","dice"+randomNumber1+".png");
 
 //right hand side image
 let randomNumber2 = Math.floor(Math.random()*(6-1+1))+1;
-document.querySelector(".img2").setAttribute("src","./images/dice"+randomNumber2+".png");
+document.querySelector(".img2").setAttribute("src","dice"+randomNumber2+".png");
 
 //logic
 if(randomNumber1>randomNumber2){
